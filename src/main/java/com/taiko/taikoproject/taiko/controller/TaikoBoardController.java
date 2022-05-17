3caf11b608b69983b856de0a4f081f2149d01812
zuzu <dlwnghks6821@naver.com>
2022-05-17 10:15:06 +0900

    pageNation 처리 해결

--- src/main/java/com/taiko/taikoproject/taiko/controller/TaikoBoardController.java
+++ src/main/java/com/taiko/taikoproject/taiko/controller/TaikoBoardController.java
@@ -53,11 +53,14 @@ public class TaikoBoardController {
     Optional<TaikoBoardEntity> entity;
 
     @GetMapping("/board")
-    public ResponseEntity getBoardList(final Pageable pageable) {
+    public ResponseEntity getBoardList(HttpServletRequest req, final Pageable pageable) {
 
-        // Page<TaikoBoardEntity> result =
-        // taikoBoard.findAll(Sort.by(Sort.Direction.DESC, "createdTime"));
-        Pageable result = PageRequest.of(0, 10, Sort.by("createdTime").descending());
+        String pageNum = req.getParameter("pageNum");
+        if (pageNum == "" || pageNum == null) {
+            pageNum = "0";
+        }
+
+        Pageable result = PageRequest.of(Integer.parseInt(pageNum), 10, Sort.by("createdTime").descending());
         return new ResponseEntity<>(taikoBoard.findAll(result), HttpStatus.OK);
 
     }
