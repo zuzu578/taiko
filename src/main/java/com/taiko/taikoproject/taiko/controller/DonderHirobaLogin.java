e872e45e56139f79715321f367b4b08b3d0c7be6
zuzu <dlwnghks6821@naver.com>
2022-05-26 01:43:07 +0900

    # 로그인시 donderhiroba 에 있는 유저 템플릿정보 가져오기

--- src/main/java/com/taiko/taikoproject/taiko/controller/DonderHirobaLogin.java
+++ src/main/java/com/taiko/taikoproject/taiko/controller/DonderHirobaLogin.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import com.taiko.taikoproject.taiko.taikoutils.TaikoHirobaLoginUtils;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- src/main/java/com/taiko/taikoproject/taiko/taikoutils/TaikoHirobaLoginUtils.java
+++ src/main/java/com/taiko/taikoproject/taiko/taikoutils/TaikoHirobaLoginUtils.java
@@ -1,43 +1,28 @@
 package com.taiko.taikoproject.taiko.taikoutils;
 
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.CookieManager;
-import com.gargoylesoftware.htmlunit.ScriptResult;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.WebRequest;
 import com.gargoylesoftware.htmlunit.html.DomElement;
-import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+
 import com.gargoylesoftware.htmlunit.html.HtmlDivision;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlImageInput;
+
 import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
-import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
-import com.gargoylesoftware.htmlunit.protocol.data.DataUrlDecoder;
-import com.gargoylesoftware.htmlunit.util.Cookie;
 
-import org.apache.tomcat.util.digester.DocumentProperties.Charset;
-import org.springframework.boot.web.servlet.server.Encoding;
-import org.springframework.web.util.DefaultUriBuilderFactory;
+import com.gargoylesoftware.htmlunit.protocol.data.DataUrlDecoder;
 
 public class TaikoHirobaLoginUtils {
     static DataUrlDecoder s;
 
     public String login(String naverId, String naverPw) throws Exception {
-
+        // HashMap<String, Object> userInfo = new HashMap<String, Object>();
         WebClient wc = new WebClient();
+        String template = "";
+
         wc.getOptions().setThrowExceptionOnScriptError(false);
         String url = "https://account.bandainamcoid.com/login.html?client_id=nbgi_taiko&customize_id=&redirect_uri=https%3A%2F%2Fwww.bandainamcoid.com%2Fv2%2Foauth2%2Fauth%3Fback%3Dv3%26client_id%3Dnbgi_taiko%26scope%3DJpGroupAll%26redirect_uri%3Dhttps%253A%252F%252Fdonderhiroba.jp%252Flogin_process.php%253Finvite_code%253D%2526abs_back_url%253D%2526location_code%253D%26text%3D&prompt=login";
 
@@ -49,11 +34,6 @@ public class TaikoHirobaLoginUtils {
             password.setValueAttribute("lms3821su");
             System.out.println("입력한 id =>" + id.getValueAttribute() + "입력한 pw =>" + password.getValueAttribute());
             password.getValueAttribute();
-            final List<?> divs = page.getByXPath("//div");
-
-            // HtmlElement button = page.getFirstByXPath("//button[@class='btn _btn-size-50
-            // _btn-gray']");
-            // button.click();
 
             HtmlElement button2 = page.getFirstByXPath("//button[@class='btn _btn-size-50 _btn-gray']");
             button2.removeAttribute("disabled");
@@ -61,22 +41,34 @@ public class TaikoHirobaLoginUtils {
             button2.removeStyleAttribute("disabled");
 
             button2.click();
-            Thread.sleep(100);
-            System.out.println(page.asNormalizedText());
-            // System.out.println(page.getTextContent());
+            // 필수!
+            Thread.sleep(5000);
 
             HtmlPage page2 = wc.getPage("https://donderhiroba.jp/login_select.php");
-            // System.out.println(page2.getTextContent());
-            // HtmlImageInput inputSubmit = (HtmlImageInput) page2
-            // .getFirstByXPath("//form[@class='buttonImage']");
-            // inputSubmit.click();
-
             System.out.println(page2.asNormalizedText());
+
+            Thread.sleep(3000);
+            HtmlAnchor button3 = page2.getAnchorByHref("javascript:void(0)");
+            button3.click();
+            // 유저 정보 페이지
+            HtmlPage page3 = wc.getPage("https://donderhiroba.jp/index.php");
+            // List<HtmlDivision> myDonImg =
+            // page3.getByXPath("//img[@class='customd_mydon']");
+            // userInfo.put("myDonImag", myDonImg.get(0));
+
+            // final List<?> divs = page.getByXPath("//div");
+            // for (int i = 0; i < divs.size(); i++) {
+            // System.out.println(divs.get(i));
+            // }
+            // List<HtmlDivision> myDongInfo = page3.getByXPath("//div[@id='mydon_area']");
+            // for (int i = 0; i < myDongInfo.size(); i++) {
+            // System.out.println(myDongInfo.get(i));
+            // }
+            template = page3.asXml();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-        // return !currPage.asText().contains("Naver Sign in");
-        return null;
+        return template;
     }
 }
